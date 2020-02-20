e8d5967b4a85d51d74bca85ad48abeb5d0dbcad6
MwasCipher <mwangicollinswaweru@gmail.com>
2020-02-20 22:35:30 +0300

    Categories

---
+++ administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2020-02-20 18:54:18 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2020-02-20T18:54:18+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
